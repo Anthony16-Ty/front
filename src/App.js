ff5aec22379073d2192cecdcc01e4962a2b1c23e
Anthony Kimeu <tkymeu@gmail.com>
2022-09-08 18:24:38 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import "./App.css";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Books from "./components/Books";
+import FormPage from "./components/FormPage";
+import { Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [books, setBooks] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  useEffect(() => {
+    fetch(``)
+      .then((response) => response.json())
+      .then((data) => {
+        setBooks(data);
+      });
+  }, []);
+
+  
+  function addBooks(newBook) {
+    const updatedBooks = [newBook, ...books];
+    setBooks(updatedBooks);
+  }
+
+  function addReview(newReview) {
+    const bookToUpdate = books.find((book) => {
+      return book.id === newReview.book_id;
+    });
+
+    const updatedReviews = [newReview, ...bookToUpdate.reviews];
+    bookToUpdate.reviews = updatedReviews;
+    setBooks(
+      books.map((book) => (book.id === bookToUpdate.id ? bookToUpdate : book))
+    );
+
+    setReviews(updatedReviews);
+  }
+
+  function handleUpdateLikes(updateLikes) {
+    const updatedLikes = books.map((book) => {
+      return book.id === updateLikes.id ? updateLikes : book;
+    });
+    setBooks(updatedLikes);
+  }
+  function handleDeleteReview(deletedReview) {
+    const bookToUpdate = books.find((book) => {
+      return book.id === deletedReview.book_id;
+    });
+
+    const updatedReview = bookToUpdate.reviews.filter((review) => {
+      return review.id !== deletedReview.id;
+    });
+
+    bookToUpdate.reviews = updatedReview;
+    setBooks(
+      books.map((book) => (book.id === bookToUpdate.id ? bookToUpdate : book))
+    );
+    setReviews(updatedReview);
+  }
+
+  const booksToDisplay = books.filter((book) => {
+    return book.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+  return (
+    <div className="App">
+      <NavBar />
+     <br />
+      <Routes>
+      <Route exact path="/books" element={
+        <Books
+          handleUpdateLikes={handleUpdateLikes}
+          addReview={addReview}
+          books={books}
+          booksToDisplay={booksToDisplay}
+          searchTerm={searchTerm}
+          onSearch={setSearchTerm}
+          handleDeleteReview={handleDeleteReview}
+          reviews={reviews}
+        />}
+      />
+      <Route exact path="/formpage" element={<FormPage  addBooks={addBooks} />} />
+      
+      <Route exact path="/" element={<Home />} />
+      
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <h1>Presto</h1>
+        <p>
+          Welcome to the presto! You can add books you have read, or
+          would like to read. You can add reviews and rate books based on how
+          you liked them!
+        </p>
+        <img
+          alt="books"
+          src="https://media.istockphoto.com/photos/open-book-picture-id152992783?k=20&m=152992783&s=612x612&w=0&h=BRdZgX9LFc1MGk6m2lM2HNC4GklxLQYFZbTw62R9Meg="
+        />
+      </div>
+    </>
+  );
+};
+
+export default Home;
