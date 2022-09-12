4a0177825f7406b2807305c4baa1c8c7b638d5be
Anthony Kimeu <tkymeu@gmail.com>
2022-09-12 14:08:12 +0300

    Edited homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,22 @@
 import React from "react";
 
-function Home()
-
-
-{
-    return (
-        <div>
-        <div className='container'>
-           <div className='row justify-content-evenly py-5'>
-           <div className='col-12 col-md-6 mt-5 mt-md-0'>
-
-
-                 <h1 className='mb-0'>PRESTO</h1>
-                 
-                 <p style={{fontWeight: 600}}>Welcome to Presto! You can books and novels you've read or you would like to read.You can rate them them based on how you like them</p>
-                </div>
-               <div className='col-sm-8 col-md-5'>
-                   <img className='img-fluid' src="https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&fit=clip" alt="about us img"/>
-               </div>
-               
-               </div>
-           </div>
-        </div>
-    );
-}
-    
-
+const Home = () => {
+  return (
+    <>
+      <div>
+        <h1>Presto</h1>
+        <p>
+          Welcome to Presto! You can add books you have read, or
+          would like to read. You can add reviews and rate books based on how
+          you liked them!
+        </p>
+        <img
+          alt="books"
+          src="https://images.unsplash.com/photo-1498906538030-8da82e61e424?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fGJvb2tzfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+        />
+      </div>
+    </>
+  );
+};
 
 export default Home;
